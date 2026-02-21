c18c74e25c5b35a371b085f83f82f282f884a61c
ezekielperez104-maker <ezekiel.perez104@gmail.com>
2026-02-21 22:07:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, REST, Routes, Collection } = require('discord.js');
+const fs = require('fs');
 
 const client = new Client({
   intents: [
@@ -8,20 +9,32 @@ const client = new Client({
   ]
 });
 
-client.once('ready', () => {
+client.commands = new Collection();
+
+// Carica i comandi
+const commandFiles = fs.readdirSync('./commands').filter(f => f.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+client.once('ready', async () => {
   console.log(`Bot online come ${client.user.tag}`);
+
+  const commands = client.commands.map(c => c.data.toJSON());
+  const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+  await rest.put(
+    Routes.applicationCommands(client.user.id),
+    { body: commands }
+  );
+  console.log('Comandi registrati!');
 });
 
-client.on('messageCreate', message => {
-  if (message.author.bot) return;
-
-  if (message.content === '!ciao') {
-    message.reply('Ciao! 👋');
-  }
-
-  if (message.content === '!ping') {
-    message.reply('Pong! 🏓');
-  }
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+  await command.execute(interaction);
 });
 
 client.login(process.env.TOKEN);
