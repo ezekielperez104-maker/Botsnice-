a61608ad16ababb44361cc0e3aad535877e1b5a4
ezekielperez104-maker <ezekiel.perez104@gmail.com>
2026-02-22 21:33:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates
   ]
 });
 
