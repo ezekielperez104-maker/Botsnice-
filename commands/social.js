adb3f65a729f9c0b6a75e973b0ce751be668fda5
ezekielperez104-maker <ezekiel.perez104@gmail.com>
2026-02-22 15:29:53 +0100

    Create social.js

---
+++ commands/social.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('social')
+    .setDescription('Mostra i social del server'),
+
+  async execute(interaction) {
+    const embed = new EmbedBuilder()
+      .setTitle('🌐 I nostri Social')
+      .setDescription('Seguici sui nostri canali social per non perderti nessun contenuto!')
+      .addFields(
+        {
+          name: '🎥 YouTube',
+          value: '[Clicca qui per visitare il canale](https://youtube.com/@iulotay)',
+          inline: true
+        },
+        {
+          name: '🎵 TikTok',
+          value: '[Clicca qui per visitare il profilo](https://www.tiktok.com/@yatolui)',
+          inline: true
+        }
+      )
+      .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/1024px-YouTube_full-color_icon_%282017%29.svg.png')
+      .setImage('https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Ionicons_logo-tiktok.svg/1200px-Ionicons_logo-tiktok.svg.png')
+      .setColor(0xFFFF00)
+      .setFooter({ text: 'Seguici per restare aggiornato! 🔔' });
+
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setLabel('▶️ YouTube')
+        .setURL('https://youtube.com/@iulotay')
+        .setStyle(ButtonStyle.Link),
+      new ButtonBuilder()
+        .setLabel('🎵 TikTok')
+        .setURL('https://www.tiktok.com/@yatolui')
+        .setStyle(ButtonStyle.Link)
+    );
+
+    interaction.reply({ embeds: [embed], components: [row] });
+  }
+};
