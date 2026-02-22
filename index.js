65b31d3653593013d0f888516ca10e68dc7bcce7
ezekielperez104-maker <ezekiel.perez104@gmail.com>
2026-02-22 12:11:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ client.on('interactionCreate', async interaction => {
       const embed = new EmbedBuilder()
         .setTitle('🎫 Ticket aperto')
         .setDescription(`Ciao ${utente}! Lo staff ti risponderà il prima possibile.\n\nPer chiudere il ticket usa il pulsante qui sotto.`)
-        .setColor(ffde21);
+        .setColor(0xFFFF00);
 
       const row = new ActionRowBuilder().addComponents(
         new ButtonBuilder()
